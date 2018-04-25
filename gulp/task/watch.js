cddfae7d3801578cdd9f3eab6f28bb210c3f1325
Robert <demond611@gmail.com>
2018-04-24 20:36:43 -0500

    updated watch.js with css tasks; updated container.css

--- gulp/task/watch.js
+++ gulp/task/watch.js
@@ -3,7 +3,7 @@ watch = require('gulp-watch'),
 browserSync = require('browser-sync').create();
 
 
-gulp.task('watch', function () {
+gulp.task( 'watch', function () {
 
 	browserSync.init({
 		notify: false,
@@ -13,8 +13,20 @@ gulp.task('watch', function () {
 	});
 
 
-	watch('./app/**/*.html', function(){
+	watch( './app/**/*.html', function () {
 		browserSync.reload();
 	});
 
+
+	watch( './app/assets/styles/**/*.css', function () {
+		gulp.start('cssInject');
+	});
+
 });
+
+
+// CSS INJECT FOR STYLES UPDATES
+	gulp.task( 'cssInject', ['styles'], function () {
+		return gulp.src( './app/temp/styles/main.css' )
+		.pipe( browserSync.stream() );
+	});
