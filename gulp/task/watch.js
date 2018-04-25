9838a9770e130998c216d9d122f04d02b481083b
Robert <demond611@gmail.com>
2018-04-24 21:57:23 -0500

    updated various files

--- gulp/task/watch.js
+++ gulp/task/watch.js
@@ -26,7 +26,7 @@ gulp.task( 'watch', function () {
 
 
 // CSS INJECT FOR STYLES UPDATES
-	gulp.task( 'cssInject', ['styles'], function () {
-		return gulp.src( './app/temp/styles/main.css' )
-		.pipe( browserSync.stream() );
-	});
+gulp.task( 'cssInject', ['styles'], function () {
+	return gulp.src( './app/temp/styles/main.css' )
+	.pipe( browserSync.stream() );
+});
